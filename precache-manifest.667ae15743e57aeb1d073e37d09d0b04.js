7a4204042e0309944b8879b222c787612ed7279e
atulsahay <atulkumars234@gmail.com>
2020-01-28 21:07:33 +0530

    Updates

---
+++ precache-manifest.667ae15743e57aeb1d073e37d09d0b04.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baecd4b6d308347ec0409a3c51d99c8b",
+    "url": "/react-projects/index.html"
+  },
+  {
+    "revision": "fd56f1ce32d25ed7dabe",
+    "url": "/react-projects/static/css/main.4decc63f.chunk.css"
+  },
+  {
+    "revision": "0ec9daa3233a94f56582",
+    "url": "/react-projects/static/js/2.7c44c9ab.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-projects/static/js/2.7c44c9ab.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd56f1ce32d25ed7dabe",
+    "url": "/react-projects/static/js/main.ff3f8c61.chunk.js"
+  },
+  {
+    "revision": "c6d5f107022de01b860e",
+    "url": "/react-projects/static/js/runtime-main.e0c044d6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-projects/static/media/logo.5d5d9eef.svg"
+  }
+]);
